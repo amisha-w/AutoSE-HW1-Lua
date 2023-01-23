4027948a50a148dbe97a1289f113d2f408e8dfdb
tilaksatra <ayushsatra@gmail.com>
2023-01-23 18:31:46 -0500

    Added Num class and methods

---
+++ src/constants.py
@@ -0,0 +1,4 @@
+import sys
+
+MAX_VALUE = sys.maxsize
+MIN_VALUE = -1 * sys.maxsize
---
+++ src/num.py
@@ -0,0 +1,24 @@
+from src.constants import *
+
+class NUM:
+    def __init__(self):
+        self.n = 0
+        self.mu = 0
+        self.m2 = 0
+        self.hi = MAX_VALUE
+        self.lo = MIN_VALUE
+
+    def add(self, n):
+        if n != "?":
+            self.n = self.n + 1
+            d = n - self.mu
+            self.mu = self.mu + (d / self.n)
+            self.m2 = self.m2 + (d * (n - self.mu))
+            self.lo = min(n, self.lo)
+            self.hi = max(n, self.hi)
+
+    def mid(self):
+        return self.mu
+
+    def div(self):
+        return (self.m2 < 0 or self.n < 2) and 0 or ((self.m2 / (self.n - 1)) ** 0.5)
